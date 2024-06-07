0266f6a378982b95b5a82a22a633d1212aecb483
유석이노트북\wnsgh <wnsghrnt2586@gmail.com>
2024-06-07 19:38:10 +0900

    가볍게 spring boot 시작

---
+++ hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -0,0 +1,18 @@
+package hello.hello_spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data","hello!!");
+        //resources/templates/hello.html를 찾아가는 문구임| 파일명기준으로 찾음
+        //컨트롤러에서 리턴 값으로 문자를 반환하면 뷰 리졸버( viewResolver )가 화면을 찾아서 처리한다.
+        return "hello";
+
+    }
+}
