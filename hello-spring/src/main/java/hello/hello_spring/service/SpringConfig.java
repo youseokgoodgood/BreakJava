d30f2a824bdbfd86b4e43e69aae0d8c923eb361c
유석이노트북\wnsgh <wnsghrnt2586@gmail.com>
2024-06-10 22:31:39 +0900

    spring boot - 간단한 회원  등록/조회

--- hello-spring/src/main/java/hello/hello_spring/service/SpringConfig.java
+++ hello-spring/src/main/java/hello/hello_spring/service/SpringConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
-    
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
