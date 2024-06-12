e891e1a4c68559df710e60eaa59f94ddbb73ef0e
유석이노트북\wnsgh <wnsghrnt2586@gmail.com>
2024-06-12 23:27:30 +0900

    spring boot - h2,jdbc 연결

--- hello-spring/src/main/java/hello/hello_spring/service/SpringConfig.java
+++ hello-spring/src/main/java/hello/hello_spring/service/SpringConfig.java
@@ -1,12 +1,23 @@
 package hello.hello_spring.service;
 
+import hello.hello_spring.repositroy.JdbcMemberRepository;
 import hello.hello_spring.repositroy.MemberRepository;
 import hello.hello_spring.repositroy.MemoryMemberRepositroy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -15,6 +26,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepositroy();
+        //return new MemoryMemberRepositroy();
+        return new JdbcMemberRepository(dataSource);
     }
 }
