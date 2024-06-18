27d7ab8c7556c1b39a6e53d2725719919652abf5
유석이노트북\wnsgh <wnsghrnt2586@gmail.com>
2024-06-18 23:20:06 +0900

    java start

---
+++ java-start/src/variable/Var6.java
@@ -0,0 +1,10 @@
+package variable;
+
+public class Var6 {
+    public static void main(String[] args){
+        int a;
+        //System.out.println(a);
+        int b = Integer.parseInt(null);
+        System.out.println(b * 1);
+    }
+}
