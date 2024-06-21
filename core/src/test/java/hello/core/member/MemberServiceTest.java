efdc52309a2f4d847bc2af07a5afc3284245a699
유석이노트북\wnsgh <wnsghrnt2586@gmail.com>
2024-06-21 17:11:54 +0900

    spring boot - 주문 도메인

---
+++ core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,22 @@
+package hello.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void 회원가입(){
+       //given
+        Member member = new Member(1L,"memberA",Grade.VIP);
+
+       //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+       //than
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
---
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,29 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void 주문생성(){
+        //given
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+
+        //when
+        memberService.join(member);
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        //than
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
+}
