5da297d636dc303ef9d26464e60287d48854a148
심유석\wnsgh <wnsghrnt2@naver.com>
2024-08-20 23:02:57 +0900

    객체지향프로그래밍-상속학습

---
+++ java-basic/src/extends1/ex1/ElectricCar.java
@@ -0,0 +1,12 @@
+package extends1.ex1;
+
+public class ElectricCar {
+
+    public void move() {
+        System.out.println("차를 이동 합니다.");
+    }
+
+    public void charge() {
+        System.out.println("충전 합니다.");
+    }
+}
