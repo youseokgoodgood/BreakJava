e9d4bc89f139845da72dcf6003cfa2f91bc408d5
심유석\wnsgh <wnsghrnt2@naver.com>
2024-08-22 22:02:45 +0900

    객체지향프로그래밍-상속기능과확장

--- java-basic/src/extends1/ex3/CarMain.java
+++ java-basic/src/extends1/ex3/CarMain.java
@@ -1,4 +1,4 @@
-package extends1.ex2;
+package extends1.ex3;
 
 public class CarMain {
     public static void main(String[] args) {
@@ -17,9 +17,16 @@ public class CarMain {
         ElectricCar electricCar = new ElectricCar();
         electricCar.move();
         electricCar.charge();
+        electricCar.openDoor();
 
         GasCar gasCar = new GasCar();
         gasCar.move();
         gasCar.fillUp();
+        gasCar.openDoor();
+
+        HydrogenCar hydrogenCar = new HydrogenCar();
+        hydrogenCar.move();
+        hydrogenCar.fillHydrogen();
+        hydrogenCar.openDoor();
     }
 }
