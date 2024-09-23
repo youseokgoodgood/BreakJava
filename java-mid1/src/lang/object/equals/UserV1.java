683b2b34414ee4b207e7b4dfc4142de8fe166b09
심유석\wnsgh <wnsghrnt2586@gmail.com>
2024-09-23 23:33:31 +0900

    Object 동일성, 동등성 비교

---
+++ java-mid1/src/lang/object/equals/UserV1.java
@@ -0,0 +1,20 @@
+package lang.object.equals;
+
+/**
+ * packageName    : lang.object.equals
+ * fileName       : UserV1
+ * author         : wnsgh
+ * date           : 2024-09-23
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-09-23        wnsgh       최초 생성
+ */
+public class UserV1 {
+    private String id;
+
+    public UserV1(String id) {
+        this.id = id;
+    }
+}
--- java-mid1/src/lang/object/tostring/ObjectPrinter.java
+++ java-mid1/src/lang/object/tostring/ObjectPrinter.java
@@ -11,6 +11,9 @@ package lang.object.tostring;
  * -----------------------------------------------------------
  * 2024-09-22        wnsgh       최초 생성
  */
+
+
+//println 작동방식 예시를 위한 간단한 구현
 public class ObjectPrinter {
     public static void print(Object obj) {
         String string = "객체 정보 출력 : " + obj.toString();
