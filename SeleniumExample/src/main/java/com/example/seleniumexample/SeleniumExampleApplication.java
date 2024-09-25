cc547c19a8598857f05e5d23a25b11b376291338
심유석\wnsgh <wnsghrnt2586@gmail.com>
2024-09-25 16:41:39 +0900

    배포버전 및 리눅스 서버 테스트 완료

--- SeleniumExample/src/main/java/com/example/seleniumexample/SeleniumExampleApplication.java
+++ SeleniumExample/src/main/java/com/example/seleniumexample/SeleniumExampleApplication.java
@@ -1,8 +1,5 @@
 package com.example.seleniumexample;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 //@SpringBootApplication
 public class SeleniumExampleApplication {
 
