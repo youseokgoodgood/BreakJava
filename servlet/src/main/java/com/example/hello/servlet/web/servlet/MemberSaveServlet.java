22f5a4c0491d787b222ce4efdf263b3f4095960c
심유석\wnsgh <wnsghrnt2586@gmail.com>
2024-10-19 11:55:35 +0900

    JSP 복습 및 서블릿 설정

---
+++ servlet/src/main/java/com/example/hello/servlet/web/servlet/MemberSaveServlet.java
@@ -0,0 +1,58 @@
+package com.example.hello.servlet.web.servlet;
+
+import com.example.hello.servlet.domain.member.Member;
+import com.example.hello.servlet.domain.member.MemberRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * packageName    : com.example.hello.servlet.web.servlet
+ * fileName       : MemberSaveServlet
+ * author         : wnsgh
+ * date           : 2024-10-19
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-10-19        wnsgh       최초 생성
+ */
+@WebServlet(name = "memberSaveServlet", urlPatterns = "/servlet/members/save")
+public class MemberSaveServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("MemberSaveServlet.service");
+        String username = req.getParameter("username");
+        int age = Integer.parseInt(req.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+        PrintWriter w = resp.getWriter();
+        w.write("<html>\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "성공\n" +
+                "<ul>\n" +
+                "    <li>id="+member.getId()+"</li>\n" +
+                "    <li>username="+member.getUsername()+"</li>\n" +
+                "    <li>age="+member.getAge()+"</li>\n" +
+                "</ul>\n" +
+                "<a href=\"/index.html\">메인</a>\n" +
+                "</body>\n" +
+                "</html>");
+
+    }
+}
