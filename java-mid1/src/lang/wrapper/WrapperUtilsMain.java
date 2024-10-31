215297c4f1c42aee072f4937f6d13860967f87fe
심유석\wnsgh <wnsghrnt2586@gmail.com>
2024-10-31 21:59:04 +0900

    래퍼클래스

---
+++ java-mid1/src/lang/wrapper/WrapperUtilsMain.java
@@ -0,0 +1,24 @@
+package lang.wrapper;
+
+/**
+ * packageName    : lang.wrapper
+ * fileName       : WrapperUtilsMain
+ * author         : wnsgh
+ * date           : 2024-10-31
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-10-31        wnsgh       최초 생성
+ */
+public class WrapperUtilsMain {
+
+    public static void main(String[] args) {
+        Integer i1 = Integer.valueOf(10);
+        Integer i2 = Integer.valueOf("10");
+        int parseInt = Integer.parseInt(i1.toString());
+
+
+
+    }
+}
