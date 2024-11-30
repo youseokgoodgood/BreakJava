8ec64e07bf71cdab8c31a25f02e5c3971a273c07
심유석\wnsgh <wnsghrnt2586@gmail.com>
2024-11-30 17:43:56 +0900

    스프링 mvc - http 응답

---
+++ springmvc/src/main/java/com/example/hello/springmvc/basic/HelloData.java
@@ -0,0 +1,19 @@
+package com.example.hello.springmvc.basic;
+
+import lombok.Data;
+
+/**
+ * author         : SYS
+ * date           : 2024-11-30
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-11-30        SYS       최초 생성
+ */
+
+@Data
+public class HelloData {
+    private String username;
+    private int age;
+}
