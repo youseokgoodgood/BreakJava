866cd49f1ff03c940bb76e01f489ea0925f96ba5
심유석\wnsgh <wnsghrnt2586@gmail.com>
2024-12-04 16:07:46 +0900

    열거형 - enum

---
+++ java-mid1/src/enumeration/ex2/ClassGrade.java
@@ -0,0 +1,20 @@
+package enumeration.ex2;
+
+/**
+ * author         : SYS
+ * date           : 2024-12-03
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-12-03        SYS       최초 생성
+ */
+public class ClassGrade {
+
+    public static final ClassGrade BASIC = new ClassGrade();
+    public static final ClassGrade GOLD = new ClassGrade();
+    public static final ClassGrade DIAMOND = new ClassGrade();
+
+    private ClassGrade() {}
+
+}
