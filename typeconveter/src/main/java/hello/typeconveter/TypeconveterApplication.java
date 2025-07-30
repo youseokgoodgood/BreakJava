df6aed30f109b32eba57010616aa1d405aa96979
youseokgoodgood <wnsghrnt2586@gmail.com>
2025-07-30 23:22:30 +0900

    스프링 타입 컨버터

---
+++ typeconveter/src/main/java/hello/typeconveter/TypeconveterApplication.java
@@ -0,0 +1,13 @@
+package hello.typeconveter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypeconveterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TypeconveterApplication.class, args);
+    }
+
+}
---
+++ typeconveter/src/main/java/hello/typeconveter/controller/HelloController.java
@@ -0,0 +1,38 @@
+package hello.typeconveter.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * packageName    : hello.typeconveter.controller
+ * fileName       : HelloController
+ * author         : sim-yuoseok
+ * date           : 2025-07-30
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2025-07-30        sim-yuoseok       최초 생성
+ */
+@Slf4j
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        String data = request.getParameter("data");//문자 타입 조회
+        Integer intValue = Integer.valueOf(data); //숫자 타입으로 변경
+
+        log.info("intValue:{}", intValue);
+        return "ok";
+    }
+
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        log.info("intValue:{}", data);
+        return "ok";
+    }
+}
