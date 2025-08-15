c51f05b1b8fade53e4393f439fdc1cb7de7bb1d2
youseokgoodgood <wnsghrnt2586@gmail.com>
2025-08-15 18:41:20 +0900

    파일 업로드

--- upload/src/main/java/hello/upload/controller/ServletUploadControllerV1.java
+++ upload/src/main/java/hello/upload/controller/ServletUploadControllerV1.java
@@ -4,11 +4,11 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.Part;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Collection;
@@ -28,7 +28,7 @@ import java.util.Collection;
 @Controller
 @RequestMapping("/servlet/v1")
 
-public class ServletUploadController {
+public class ServletUploadControllerV1 {
 
     @GetMapping("/upload")
     public String newFile() {
@@ -47,4 +47,6 @@ public class ServletUploadController {
 
         return "upload-form";
     }
+
 }
+
